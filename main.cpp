c7de27bf214d46d557e64fcaab78a39b4f92f279
JordanDeason <deasjr17@wfu.edu>
2019-09-05 15:44:03 -0400

    Finished task 2
    Signed-off-by: JordanDeason <deasjr17@wfu.edu>

--- main.cpp
+++ main.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    const int NUM_ELEMENTS = 8;   // Number of elements
-    int revVctr[8];               // User values
+    const int NUM_ELEMENTS = 8; // Number of elements
+    vector<int> revVctr(8); // User values
     int i;                        // Loop index
 
     cout << "Enter " << NUM_ELEMENTS << " integer values..." << endl;
     for (i = 0; i < NUM_ELEMENTS; ++i) {
-        cin >> revVctr[i];
+        cin >> revVctr.at(i);
     }
 
     // Reverse
     int tempVar;
     for (i = 0; i < NUM_ELEMENTS / 2; ++i) {
-        tempVar = revVctr[i];
-        revVctr[i] = revVctr[NUM_ELEMENTS - i - 1];// Swap
-        revVctr[NUM_ELEMENTS - i - 1] = tempVar;
+        tempVar = revVctr.at(i);
+        revVctr.at(i) = revVctr.at(NUM_ELEMENTS - i - 1);// Swap
+        revVctr.at(NUM_ELEMENTS - i - 1) = tempVar;
     }
 
     // Print values
